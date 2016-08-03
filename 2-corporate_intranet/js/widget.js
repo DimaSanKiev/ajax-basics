459f50489ea6dadecc2aca298247b7237a7cce5b
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-08-04 00:45:40 +0300

    processing JSON data

--- 2-corporate_intranet/js/widget.js
+++ 2-corporate_intranet/js/widget.js
@@ -5,7 +5,23 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange() = function () {
     if (xhr.readyState === 4) {
         var employees = JSON.parse(xhr.responseText);
-        console.log(employees);
+        var statusHTML = '<ul class="bulleted>';
+        for (var i = 0; i < employees.length; i++) {
+            // check the "inoffice" property
+            if (employees[i].inoffice === true) {
+                // create a new HTML list item with class 'in'
+                statusHTML += '<li class="in">';
+            } else {
+                // create a new HTML list item with class 'out'
+                statusHTML += '<li class="out">';
+            }
+            // get the value for the "name" property; insert it inside the <li> tag
+            statusHTML += employees[i].name;
+            // close the <li> tag
+            statusHTML += '</li>';
+        }
+        statusHTML += '</ul>';
+        document.getElementById('employeeList').innerHTML = statusHTML;
     }
 };
 
